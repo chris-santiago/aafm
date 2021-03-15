3316214be7110898a8dba1aa2ef5aa175efc05c8
csantiago37 <cjsantiago@gatech.edu>
2021-03-15 19:30:33 -0400

    add functions to clean daily data

--- dva/clean.py
+++ dva/clean.py
@@ -1,6 +1,5 @@
 import pandas as pd
-
-prices = pd.read_parquet('./data/fund_prices.parq')
+import pathlib
 
 
 def get_monthly_prices(data: pd.DataFrame) -> pd.DataFrame:
@@ -19,3 +18,12 @@ def get_longest_series(data: pd.DataFrame) -> pd.DataFrame:
         least_missing = df.loc[fund].isna().sum(axis=1).sort_values().index[0]
         longest.append(df.loc[(fund, least_missing), :])
     return pd.concat(longest, axis=1)
+
+
+if __name__ == '__main__':
+    here = pathlib.Path(__file__).parent
+    project = here.parent
+    file_path = project.joinpath('data', 'fund_prices.parq')
+    prices = pd.read_parquet(file_path)
+    file_path = project.joinpath('data', 'monthly_prices.csv')
+    get_longest_series(get_monthly_prices(prices)).to_csv(file_path)
