2e990f3dac97b81727b5848ad767516ebcec62bc
csantiago37 <cjsantiago@gatech.edu>
2021-03-20 09:10:52 -0400

    add single plots for mds, isomap; refactor map_returns

--- dva/map_returns.py
+++ dva/map_returns.py
@@ -7,28 +7,36 @@ from sklearn.manifold import Isomap, MDS, TSNE
 
 
 def map_to_lower_dim(data: pd.DataFrame, method: str) -> pd.DataFrame:
-
     func = {
         'isomap': Isomap(n_neighbors=5, n_components=2),
         'mds': MDS(metric=False),
         'tsne': TSNE(n_components=2, perplexity=10)
     }
 
+    mapped = data.copy()
+    df_num = mapped.iloc[:, 10:]
+    pl = Pipeline(
+        steps=[
+            ('scaler', StandardScaler()),
+            ('encode', func[method]),
+        ]
+    )
+    components = pl.fit_transform(df_num)
+    mapped['x_coord'] = components[:, 0]
+    mapped['y_coord'] = components[:, 1]
+    return mapped
+
+
+def map_by_group(data: pd.DataFrame, method: str, group: str) -> pd.DataFrame:
+    mask = data['userCategory'] == group
+    df = data[mask].copy()
+    return map_to_lower_dim(df, method)
+
+
+def map_all_by_group(data: pd.DataFrame, method: str) -> pd.DataFrame:
     all_data = []
-    for cat in data['userCategory'].unique():
-        mask = data['userCategory'] == cat
-        df = data[mask].copy()
-        df_num = df.iloc[:, 10:]
-        pl = Pipeline(
-            steps=[
-                ('scaler', StandardScaler()),
-                ('encode', func[method]),
-            ]
-        )
-        components = pl.fit_transform(df_num)
-        df['x_coord'] = components[:, 0]
-        df['y_coord'] = components[:, 1]
-        all_data.append(df)
+    for group in data['userCategory'].unique():
+        all_data.append(map_by_group(data, method, group))
     return pd.concat(all_data)
 
 
