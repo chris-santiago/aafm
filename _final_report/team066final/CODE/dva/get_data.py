4582f89b891169ecb6517055c411ab9ce28e6006
chris-santiago <cjsantiago@gatech.edu>
2021-04-20 19:54:11 -0400

    fix main notebook; add fecha to raw files

--- _final_report/team066final/CODE/dva/get_data.py
+++ _final_report/team066final/CODE/dva/get_data.py
@@ -1,6 +1,5 @@
 from typing import List
 
-import json
 import requests
 import pandas as pd
 import pathlib
@@ -12,10 +11,12 @@ def get_dates(start_date: str, end_date: str) -> List[str]:
     return [x.strftime('%Y%m%d') for x in pd.date_range(start_date, end_date, freq='D')]
 
 
-def pull_data(date:str , output_dir: pathlib.Path):
+def pull_data(date: str, output_dir: pathlib.Path):
     resp = requests.get(f"https://api.aafm.cl/publicDailyStatistics/{date}")
     if resp.ok:
-        pd.DataFrame(resp.json()).to_csv(output_dir.joinpath(f'mutual_fund_data_{date}.csv'), index=False)
+        df = pd.DataFrame(resp.json())
+        df['fecha'] = date
+        df.to_csv(output_dir.joinpath(f'mutual_fund_data_{date}.csv'), index=False)
 
 
 def pull_all(dates, output_dir):
